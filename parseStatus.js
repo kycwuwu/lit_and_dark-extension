59006125a1e440e467b4aaf0c4aa6da49bee63a7
Kylie Wu <kcw2141@columbia.edu>
2019-12-16 16:15:21 -0500

    Script can now parse out texts on /web/timelines/home

--- parseStatus.js
+++ parseStatus.js
@@ -1,12 +1,41 @@
-console.log(document);
-var mastodon = document.getElementById("mastodon");
-console.log("MASTODON:", mastodon);
+'use strict';
 
-if (mastodon) { 
-    mastodon.addEventListener('load', function () {
-      console.log("this page loaded");
-      console.log(document);
+function parseMessages(feed) {
+    var result = [];
+
+    Array.from(feed).forEach((msg) => {
+        //console.log(msg);
+        var msgId = msg.getAttribute('data-id');
+        //console.log('id:', msgId);
+        var contentWrapper = msg.getElementsByClassName('status__content__text');
+        //console.log('contentWrapper', contentWrapper);
+
+        if (Array.from(contentWrapper).length > 0) {
+            var msgWrapper = contentWrapper[0].getElementsByTagName('p')[0];
+            //console.log('msgWrapper:', msgWrapper);
+            var msgText = msgWrapper.innerHTML;
+            console.log(msgId, msgText);
+
+            result.push({'id': msgId, 'content': msgText});
+        }
     });
+
+    return result;
+}
+
+var mastodon = document.getElementById("mastodon");
+console.log("[L&D] MASTODON:", mastodon);
+
+if (mastodon) {
+      console.log("[L&D] this page loaded");
+      var feedMessages = mastodon.getElementsByTagName('article') || [];
+      console.log("feed:", feedMessages);
+      var requestList = parseMessages(feedMessages);
+      console.log(requestList);
+
+      // TODO: Make API call with requestList in JSON body
+
+      // TODO: Take response and then toggle visibility of messages with bad content
 } else {
-    console.log("no mastodon found");
+    console.log("[L&D] no mastodon found");
 }
