ac8f988107abea1a18db0ecd5b71e338a65cab55
Kylie Wu <kcw2141@columbia.edu>
2019-12-17 11:27:28 -0500

    Hitting API

--- parseStatus.js
+++ parseStatus.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var url = 'https://api.litndark.xyz/analyze/';
+
 function parseMessages(feed) {
     var result = [];
 
@@ -23,19 +25,62 @@ function parseMessages(feed) {
     return result;
 }
 
+function parseResponse(response) {
+    if (response.ok) {
+        return response.text();
+    } else {
+        return Promise.reject(new Error(response.statusText));
+    }
+}
+
+async function predictBullying(request) {
+    try {
+        console.log(request);
+        const response = await fetch(url + request.content, {
+            'mode': 'no-cors',
+            'headers': {
+                'Access-Control-Allow-Origin': 'https://api.litndark.xyz/analyze/*'
+            }
+        });
+        const json = await response.text();
+        console.log('Success:', JSON.stringify(json));
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
 var mastodon = document.getElementById("mastodon");
-console.log("[L&D] MASTODON:", mastodon);
 
 if (mastodon) {
-      console.log("[L&D] this page loaded");
-      var feedMessages = mastodon.getElementsByTagName('article') || [];
-      console.log("feed:", feedMessages);
-      var requestList = parseMessages(feedMessages);
-      console.log(requestList);
+    console.log("[L&D] this page loaded");
+    var feedMessages = mastodon.getElementsByTagName('article') || [];
+    console.log("feed:", feedMessages);
+    var requestList = parseMessages(feedMessages);
+    console.log(requestList);
 
-      // TODO: Make API call with requestList in JSON body
+    // TODO: Make API call with requestList in JSON body
+    var urlList = requestList.map((request) => { 
+        return { ...request, 'apiUrl': url + encodeURI(request.content) };
+    });
+    console.log('Req List with API URLS', urlList);
 
-      // TODO: Take response and then toggle visibility of messages with bad content
+    //var promiseList = promiseList.map((request) => predictBullying(request));
+    
+    // TODO: Add visibility attribute to object based on API response
+    // Reflect this visibility in DOM
+    Promise.all(urlList.map(req => {
+        fetch(req.apiURL, {'mode': 'no-cors'})
+            .then(parseResponse)
+            .catch(console.log('ERROR'));
+        }
+    )).then(() => {
+        // Toggle visibility
+        console.log('success');
+    });
+
+    /*requestList.forEach((request) => {
+        predictBullying(request);
+    });*/
 } else {
-    console.log("[L&D] no mastodon found");
+    console.log("[L&D] no mastodon content found");
 }
